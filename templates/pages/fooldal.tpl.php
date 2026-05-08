6581c24a66b791d6ec078b9ddf09dd45fefbe8b4
ItsKevin95 <137278306+ItsKevin95@users.noreply.github.com>
2026-05-08 20:11:57 +0200

    Create fooldal.tpl.php

---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,46 @@
+<h2>Üdvözöljük a Mozi webalkalmazásban!</h2>
+
+<p>
+    A webalkalmazás egy PHP alapú mozi adatbázis rendszer, amely lehetőséget biztosít
+    filmek kezelésére, képgaléria használatára, kapcsolatfelvételre és adatbázis alapú CRUD
+    műveletekre.
+</p>
+
+<p>
+    Az alkalmazás reszponzív kialakítású, így mobil eszközökön is megfelelően használható.
+</p>
+
+<div class="video-container">
+
+    <h3>Saját videó</h3>
+
+    <video width="100%" controls>
+        <source src="videos/mozi.mp4" type="video/mp4">
+        A böngésző nem támogatja a videó lejátszást.
+    </video>
+
+</div>
+
+<div class="video-container">
+
+    <h3>YouTube videó</h3>
+
+    <iframe
+        src="https://www.youtube.com/embed/EXeTwQWrcwY"
+        title="YouTube video"
+        allowfullscreen>
+    </iframe>
+
+</div>
+
+<div class="video-container">
+
+    <h3>Elérhetőség</h3>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.308998531765!2d19.68968627688297!3d46.90617433650598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743dcb3c0cf0cb9%3A0x94f1f2ef4a15d7fd!2sKecskem%C3%A9t!5e0!3m2!1shu!2shu!4v1710000000000!5m2!1shu!2shu"
+        allowfullscreen=""
+        loading="lazy">
+    </iframe>
+
+</div>
